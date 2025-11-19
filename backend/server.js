235b468e01057c4078894c1ff6a0fe5881b31a93
DanDavD <danielfdd2004@gmail.com>
2025-11-19 14:27:21 -0600

    endpoint usuario

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,19 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+console.log("Cargando rutas desde:", __dirname);
+console.log("Intentando cargar archivo: ./routes/usuarios.routes.js");
+
 app.use(express.json());
 
 // Endpoint de prueba
 app.get("/", (req, res) => {
   res.send("Backend funcionando correctamente 🚀");
 });
+
+//endpoint de usuarios
+const usuariosRoutes = require("./routes/usuarios.routes");
+app.use("/api", usuariosRoutes);
 
 // Aquí crearás tus endpoints reales
 app.get("/clientes", (req, res) => {
