0317a261ffa4d02ea3f40809caecb3bbacb45eb9
Rpaz17 <rebeca.paz@unitec.edu>
2025-11-25 23:20:07 -0600

    Changes in routes

--- backend/routes/apartamentos.routes.js
+++ backend/routes/apartamentos.routes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 
-const { crearApartamento, getApartamentos, getApartamentosById, eliminarApartamento } = require("../controllers/apartamentos.controller");
+const { crearApartamento, getApartamentos, getApartamentosById, editarApartamento, eliminarApartamento } = require("../controllers/apartamentos.controller");
 
 router.post("/apartamentos", crearApartamento);
 router.get("/getApt", getApartamentos);
 router.get("/getAptById/:id", getApartamentosById);
+router.put("/apartamentos/:id", editarApartamento);
 router.delete("/apartamentos/:id", eliminarApartamento);
 
 module.exports = router;
