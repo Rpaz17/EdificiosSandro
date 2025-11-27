180c2de1d5421a66e63f78e2f2c734180941cca2
sandro-70 <fernandezsandro1604@gmail.com>
2025-11-26 23:14:48 -0600

    commit

--- backend/routes/clientes.routes.js
+++ backend/routes/clientes.routes.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 // Importar controlador
-const { crearCliente } = require("../controllers/clientes.controller");
+
+const clientesController = require("../controllers/clientes.controller");
 
 // POST /api/clientes >>  user controller
-router.post("/clientes", crearCliente);
+router.post("/clientes", clientesController.crearCliente);
+router.patch("/clientes/:id", clientesController.editarCliente);
+router.delete("/clientes/:id", clientesController.eliminarCliente);
 
 module.exports = router;
