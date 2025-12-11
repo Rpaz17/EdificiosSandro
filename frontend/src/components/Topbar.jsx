f819f5da78c0b4db72921605d3395358d93f61b5
Rpaz17 <rebeca.paz@unitec.edu>
2025-12-10 23:15:57 -0600

    Notificaciones
    el overlay de notificacion funciona pero aun hay algunos problemas con mostrar el detalle de cada notificacion

--- frontend/src/components/Topbar.jsx
+++ frontend/src/components/Topbar.jsx
@@ -1,9 +1,15 @@
 import { Bell, User, LogOut, ChevronDown } from "lucide-react";
+import { NotificationsDropdown } from "./notificacionDropdown";
 
 export default function Topbar(props) {
   const {
     onUserClick, //Para ver info del usuario actual
-    onNotificationClick, //Para cuando tenga funcionalidad las notificaciones
+    onNotificationsToggle,
+    isNotificationsOpen,
+    unreadCount, 
+    notifications, 
+    onNotificationClick,
+    onMarkAllAsRead
   } = props;
   return (
     <header className="h-16 bg-white border-b border-gray-200 flex items-center justify-between px-6">
@@ -11,12 +17,31 @@ export default function Topbar(props) {
         <h1 className="text-gray-900">Panel de Control</h1>
       </div>
       <div className="flex items-center gap-4">
-        {/* Notifications */}
-        <div className="relative">
-          <button className="relative p-2 text-gray-500 hover:bg-gray-100 rounded-lg transition-colors">
-            <Bell className="w-5 h-5" />
-          </button>
-        </div>
+      {/* Zona de Notificaciones (es 'relative') */}
+      <div className="relative"> 
+        <button 
+          onClick={onNotificationsToggle} 
+          className={`relative p-2 text-gray-500 rounded-lg transition-colors ${
+            isNotificationsOpen ? 'bg-gray-100 text-blue-600' : 'hover:bg-gray-100'
+          }`}
+        >
+          <Bell className="w-5 h-5" />
+          
+          {unreadCount > 0 && (
+            <span className="absolute top-1 right-1 h-2 w-2 rounded-full bg-red-500 border border-white" />
+          )}
+        </button>
+
+        {/* AHORA SE RENDERIZA AQUÍ, DENTRO DEL CONTENEDOR RELATIVE */}
+        {isNotificationsOpen && (
+            <NotificationsDropdown
+                notifications={notifications}
+                onClose={onNotificationsToggle} 
+                onNotificationClick={onNotificationClick}
+                onMarkAllAsRead={onMarkAllAsRead}
+            />
+        )}
+      </div>
 
         {/* User Menu */}
         <div className="relative">
