6139578cfcee0950db425b67ad814e1234196c31
Rpaz17 <rebeca.paz@unitec.edu>
2025-12-17 16:35:11 -0600

    Cambios de mayusculas

--- frontend/src/pages/apartamentos.jsx
+++ frontend/src/pages/apartamentos.jsx
@@ -144,11 +144,11 @@ export function Apartamentos() {
 
   const getEstadoBadge = (estado) => {
     switch (estado) {
-      case "Ocupado":
+      case "ocupado":
         return "bg-blue-100 text-blue-800";
-      case "Disponible":
+      case "disponible":
         return "bg-green-100 text-green-800";
-      case "Mantenimiento":
+      case "mantenimiento":
         return "bg-yellow-100 text-yellow-800";
       default:
         return "bg-gray-100 text-gray-800";
